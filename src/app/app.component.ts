e86a067e1e257a3343eb905f5f69b554c54b2a61
mr-rampage <readonly@che.wbac.ca>
2019-04-15 23:07:40 -0400

    refactor(clean up)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div style="text-align:center">
+      <app-wikipedia-container></app-wikipedia-container>
+    </div>
+  `
 })
 export class AppComponent {
   title = 'app';
