85065938d851816abf78cad21c595d522d7d044d
mr-rampage <readonly@che.wbac.ca>
2019-04-16 21:49:00 -0400

    feat(store)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {AppComponent} from './app.component';
 import {WikipediaModule} from './wikipedia/wikipedia.module';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 import {environment} from '../environments/environment';
+import {StoreModule} from '@ngrx/store';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import {environment} from '../environments/environment';
   imports: [
     BrowserModule,
     WikipediaModule,
+    StoreModule.forRoot({}),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
