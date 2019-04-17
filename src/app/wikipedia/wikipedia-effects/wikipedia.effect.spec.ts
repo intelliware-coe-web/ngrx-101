91b1d86ac73918393cffc5f56c04411c2f7df811
mr-rampage <readonly@che.wbac.ca>
2019-04-17 14:09:14 -0400

    fix

---
+++ src/app/wikipedia/wikipedia-effects/wikipedia.effect.spec.ts
@@ -0,0 +1,3 @@
+describe('Wikipedia Effect', () => {
+
+});
---
+++ src/app/wikipedia/wikipedia-search-results.model.ts
@@ -0,0 +1,7 @@
+export class WikipediaSearchResults {
+  constructor(
+    readonly href: URL,
+    readonly title: string,
+    readonly description: string
+  ) {}
+}
